78bdd0142875301ad5818f4294f27dec8ebc5d64
croko22 <kev132011@hotmail.com>
2023-10-09 19:13:41 -0500

    f: Test file

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// TEMPLATE
+#define TxtIO                         \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    TxtIO;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    int n;
+
+    return 0;
+}
